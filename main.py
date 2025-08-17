2d410083aa22c5cdc906da4a0b6fcb7152844a57
Vadim Vasilev <vadiaanx@mail.ru>
2025-08-18 01:18:40 +0300

    Finish 10.1 lesson Add new Readme.md

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Изменения
-# Комментарии
+from datetime import datetime
+
+import date from date
+
+events = [
+    {'name': 'Встреча', 'date': '2024-02-01'},
+    {'name': 'День рождения', 'date': '2025-01-01'},
+    {'name': 'Отпуск', 'date': '2023-08-10'}
+]
+datetime.fromisoformat(key=lambda x: x["date"])
+print(events)
